aa1f1426edf0e341e0c84ad3deb49cf15796dae7
Miles Oldenburg <milesoldenburg@gmail.com>
2015-03-29 18:56:58 -0400

    Sending track list over web socket and displaying in browser

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,32 @@
-var socket = io.connect('http://localhost:3000');
+require.config({
+    'baseUrl' : 'js/lib',
+    'paths' : {
+        'bootstrap' : 'bootstrap/bootstrap.min',
+        'html' : '../../html',
+        'jquery' : 'jquery/jquery.min',
+        'mustache' : 'mustache/mustache.min',
+        'socketio' : 'socket.io-client/socket.io',
+        'text' : 'text/text'
+    },
+    'shim' : {
+        'bootstrap' : {
+            'deps' : ['jquery']
+        },
+        'socketio' : {
+            'exports' : 'io'
+        }
+    },
+    'urlArgs' : 'bust=' + (new Date()).getTime()
+});
 
-socket.on('news', function(data){
-    console.log(data);
+require(['socketio', 'mustache', 'bootstrap'], function(io, Mustache){
+    var socket = io.connect('http://localhost:3000');
+
+    socket.on('tracklist', function(data){
+        $(document).ready(function(){
+            require(['text!html/tracks.html'], function(TracksTemplate){
+                $('.page-container').html(Mustache.render(TracksTemplate, data));
+            });
+        });
+    });
 });
